993d866cb44fa462a0068b38469308735c4f1f47
Carlos Calderon <cdcalderon@gmail.com>
2022-09-11 19:21:12 -0500

    implement angular project -> blockchain contract connection smoke test

--- bc/scripts/1_deploy.ts
+++ bc/scripts/1_deploy.ts
@@ -1,14 +1,18 @@
-import { Token } from './../typechain-types/Token';
+//import { Token } from './../typechain-types/Token';
 import { ethers } from 'hardhat';
+import fs from 'fs';
 
 async function main() {
   //Fetch contract to deploy
   const Token = await ethers.getContractFactory('Token');
 
   // Deploy contract
-  const token = await Token.deploy();
+  const token = await Token.deploy('Poncho', 'PCHO', '1000000');
   await token.deployed();
   console.log(`Token deployed to: ${token.address}`);
+
+  let addressesJSON = JSON.stringify({ tokenContract: token.address });
+  fs.writeFileSync('../src/environments/contract-address.json', addressesJSON);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { ethers } from 'ethers';
+
+declare let window: any;
+import { Component, OnInit } from '@angular/core';
+import Token from '../../bc/artifacts/contracts/Token.sol/Token.json';
+import addresses from '../environments/contract-address.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'exchangeme';
+export class AppComponent implements OnInit {
+  signer: any;
+  tokenContract: any;
+  signerAddress: any;
+
+  async ngOnInit() {
+    const provider = this.loadProvider();
+    await provider.send('eth_requestAccounts', []); // <- this promps user to connect metamask
+    // Reload page when network changes
+
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload();
+    });
+
+    // Fetch current account & balance from Metamask when changed
+    window.ethereum.on('accountsChanged', () => {
+      this.loadAccount(provider);
+    });
+    // const provider = new ethers.providers.Web3Provider(window.ethereum, 'any');
+
+    // const connection = this.loadProvider();
+    const chainId = await this.loadNetwork(provider);
+    console.log(chainId);
+
+    provider.on('network', (newNetwork: any, oldNetwork: any) => {
+      if (oldNetwork) {
+        window.location.reload();
+      }
+    });
+
+    this.signer = provider.getSigner();
+    console.log('carlos Provider  ', this.signer);
+
+    console.log('carlos Provider  ', await this.signer.getChainId());
+    // if ((await this.signer.getChainId()) !== 80001) {
+    //   alert('Please change your network to ');
+    // }
+
+    console.log('address from config ', addresses.tokenContract);
+    this.tokenContract = new ethers.Contract(
+      addresses.tokenContract,
+      Token.abi,
+      this.signer
+    );
+
+    console.log(this.tokenContract);
+    const symbol = await this.tokenContract.name();
+
+    // const tokenName = await this.tokenContract.name();
+    // console.log('token name: ', tokenName);
+    console.log('token symbol: ', symbol);
+  }
+
+  loadProvider() {
+    const connection = new ethers.providers.Web3Provider(window.ethereum);
+    return connection;
+  }
+
+  async loadNetwork(provider: any) {
+    const { chainId } = await provider.getNetwork();
+    return chainId;
+  }
+
+  async loadAccount(provider: any) {
+    const accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    });
+    const account = ethers.utils.getAddress(accounts[0]);
+
+    let balance = await provider.getBalance(account);
+    balance = ethers.utils.formatEther(balance);
+
+    return account;
+  }
+
+  // async loadTokens(provider: any, address: any) {
+  //   let token, symbol;
+
+  //   token = new ethers.Contract(address, Token.abi, provider);
+  //   symbol = await token.symbol();
+
+  //   return token;
+  // }
 }
