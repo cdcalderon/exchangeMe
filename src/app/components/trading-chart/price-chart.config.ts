cda2edb29d114ce87592ba47d79d203f30985405
Carlos Calderon <cdcalderon@gmail.com>
2022-10-07 17:56:13 -0500

    refactor char dependencies

---
+++ src/app/components/trading-chart/price-chart.config.ts
@@ -0,0 +1,20 @@
+export const options = {
+  chart: {
+    type: 'candlestick',
+    height: 350,
+  },
+  title: {
+    text: 'Carlos Exchnage Chart',
+    align: 'left',
+  },
+  xaxis: {
+    type: 'datetime',
+  },
+  yaxis: {
+    tooltip: {
+      enabled: true,
+    },
+  },
+};
+
+export const defaultSeries = [];
