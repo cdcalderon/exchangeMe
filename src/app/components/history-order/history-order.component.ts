9ff431dd37cc83a129d8dee8e7acef46366427c2
Carlos Calderon <cdcalderon@gmail.com>
2022-10-06 21:53:28 -0500

    wire up cancel order event

--- src/app/components/history-order/history-order.component.ts
+++ src/app/components/history-order/history-order.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable, take } from 'rxjs';
+import { ExchangeService } from 'src/app/shared/services/exchange.service';
+import { EventAggregator } from 'src/app/shared/services/helpers/event-aggregator';
 import * as fromStore from 'src/app/store/app.reducer';
 
 @Component({
@@ -12,11 +14,26 @@ export class HistoryOrderComponent implements OnInit {
   myOpenOrders$: Observable<any>; // TODO: create interfaces
   myFilledOrders$: Observable<any>; // TODO: create interfaces
   symbols$: Observable<string[]>;
-  constructor(private store: Store<fromStore.AppState>) {}
+  constructor(
+    private store: Store<fromStore.AppState>,
+    private exchangeService: ExchangeService,
+    private eventAggregator: EventAggregator
+  ) {}
 
   ngOnInit(): void {
     this.symbols$ = this.store.select(fromStore.getSymbolsSelector);
     this.myOpenOrders$ = this.store.select(fromStore.myOpenOrdersSelector);
     this.myFilledOrders$ = this.store.select(fromStore.myFilledOrdersSelector);
+  }
+
+  cancelOrder(order) {
+    const exchange$ = this.eventAggregator.exchange;
+    const provider$ = this.eventAggregator.providerConnection;
+
+    combineLatest([provider$, exchange$])
+      .pipe(take(1))
+      .subscribe((result) => {
+        this.exchangeService.cancelOrder(result[0], result[1], order);
+      });
   }
 }
