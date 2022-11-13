57efb7c6d2e8cb10cd2aadb9cc4a0bbe0a32bd36
Carlos Calderon <cdcalderon@gmail.com>
2022-11-13 14:25:43 -0600

    update deploy script and settings

--- bc/hardhat.config.ts
+++ bc/hardhat.config.ts
@@ -1,10 +1,60 @@
 import { HardhatUserConfig } from 'hardhat/config';
+require('dotenv').config();
 import '@nomicfoundation/hardhat-toolbox';
+
+const privateKeys = process.env.PRIVATE_KEYS || '';
+const MAINNET_RPC_URL =
+  process.env.MAINNET_RPC_URL ||
+  process.env.ALCHEMY_MAINNET_RPC_URL ||
+  'https://eth-mainnet.alchemyapi.io/v2/your-api-key';
+const GOERLI_RPC_URL =
+  process.env.GOERLI_RPC_URL ||
+  'https://eth-goerli.alchemyapi.io/v2/your-api-key';
+const POLYGON_MAINNET_RPC_URL =
+  process.env.POLYGON_MAINNET_RPC_URL ||
+  'https://polygon-mainnet.alchemyapi.io/v2/your-api-key';
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '0x';
+// optional
+const MNEMONIC = process.env.MNEMONIC || 'your mnemonic';
+
+// Your API key for Etherscan, obtain one at https://etherscan.io/
+const ETHERSCAN_API_KEY =
+  process.env.ETHERSCAN_API_KEY || 'Your etherscan API key';
+const POLYGONSCAN_API_KEY =
+  process.env.POLYGONSCAN_API_KEY || 'Your polygonscan API key';
+const REPORT_GAS = process.env.REPORT_GAS || false;
 
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
   networks: {
-    localhost: {},
+    hardhat: {
+      // // If you want to do some forking, uncomment this
+      // forking: {
+      //   url: MAINNET_RPC_URL
+      // }
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: privateKeys.split(','),
+      //   accounts: {
+      //     mnemonic: MNEMONIC,
+      //   },
+    },
+    mainnet: {
+      url: MAINNET_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //   accounts: {
+      //     mnemonic: MNEMONIC,
+      //   },
+    },
+    polygon: {
+      url: POLYGON_MAINNET_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+    },
   },
 };
 
